35026741dfda8475e9643f140e187181cf5f122e
Abey Onalaja <abey.onalaja@gmail.com>
2016-11-29 11:06:03 +0000

    Add cutom textfield

---
+++ LoginScreen/LoginTextField.swift
@@ -0,0 +1,28 @@
+//
+//  LoginTextField.swift
+//  LoginScreen
+//
+//  Created by Abideen Onalaja on 29/11/2016.
+//  Copyright © 2016 Glow. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LoginTextField: UITextField {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.layer.borderColor = UIColor(white: 231 / 255, alpha: 1).cgColor
+        self.layer.borderWidth = 1
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 8, dy: 7)
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return textRect(forBounds: bounds)
+    }
+}
